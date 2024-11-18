f9cafbd8e86664b9d0bf54ffde9499056e7a9ecb
Berhan Cabuk <berhan.cabuk@student.hs-rm.de>
2024-11-18 20:08:59 +0100

    Blatt 4 aufgabe 1 done

--- Blatt4/au1.c
+++ Blatt4/au1.c
@@ -2,31 +2,64 @@
 #include <string.h>
 #include <stdlib.h>
 
+enum
+{
+    LAENGE = 25
+};
+struct Mensch
+{
+    char name[LAENGE];
+    struct Mensch *next;
+} *mokel, *merke = NULL;
+
 int main(int argc, char *argv[])
 {
-    struct Mensch
-    {
-        char name[25];
-        struct Mensch *next;
-    } *mokel, *merke = NULL;
-
-    char eingabe[25];
+    char eingabe[LAENGE];
 
     while (scanf("%s", eingabe) != EOF)
     {
         mokel = malloc(sizeof(struct Mensch));
+        if (!mokel)
+            return 1;
 
-        strncpy(mokel->name, eingabe, 25);
+        strcpy(mokel->name, eingabe);
 
+        /*Hier wird der erste Mensch eingesetzt*/
         if (merke == NULL)
         {
             merke = mokel;
             merke->next = merke;
-        }else{
+        }
+        else
+        {
             merke->next = mokel;
             mokel->next = merke;
         }
+        merke = mokel;
     }
-    free(mokel);
+    if (mokel != NULL)
+    {
+        argc--;
+        do
+        {
+            mokel = merke->next;
+            printf("%s\n",mokel->name);
+
+            merke->next = merke->next;
+            free(mokel);
+            argc--;
+        } while (merke != mokel && argc > 0 );
+        while (merke != mokel)
+        {
+            mokel = merke->next;
+            merke->next = mokel->next;
+            free(mokel);
+        }
+    }
+    else
+    {
+        printf("Bitte Namen eingeben:\n");
+    }
+
     return 0;
 }
