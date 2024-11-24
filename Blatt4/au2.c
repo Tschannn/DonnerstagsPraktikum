cbeb9af0f09a5d83ce2042a8642e67feedbbe526
Berhan Cabuk <berhan.cabuk@student.hs-rm.de>
2024-11-24 14:36:45 +0100

    push halt

--- Blatt4/au2.c
+++ Blatt4/au2.c
@@ -1,60 +1,69 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-typedef struct Node{
+typedef struct Node
+{
     char *data;
     struct Node *next;
     struct Node *previous;
-}*nodep;
+} *nodep;
 
-void printList(nodep lst){
+void printList(nodep lst)
+{
     while (lst->next != NULL)
     {
-    printf("%p\n",lst);
-    lst = lst->next;
+        printf("%p\n", lst);
+        lst = lst->next;
     }
 }
 
-nodep insertAt(nodep lst, int pos,char *inhalt) {
+nodep insertAt(nodep lst, int pos, char *inhalt)
+{
     int i;
 
     nodep newP;
 
     newP = malloc(sizeof(struct Node));
-    if(!newP){
+    if (!newP)
+    {
         return 1;
     }
 
     newP->data = inhalt;
 
-    if(lst == NULL){
+    if (lst == NULL)
+    {
         lst = newP;
         lst->next = NULL;
         lst->previous = NULL;
-    }else{
-        if(pos == -1){
-          
-        }
-        if(pos == 0){
-        newP = lst->previous;
-         
-        }
-    for(i=0;i<pos;i++){
-        lst = lst->next;
-        if(i == pos){
-         lst->data = inhalt;
-        }
-    }   
     }
-
+    else
+    {
+        if (pos == -1)
+        {
+        }
+        if (pos == 0)
+        {
+            newP = lst->previous;
+        }
+        for (i = 0; i < pos; i++)
+        {
+            lst = lst->next;
+            if (i == pos)
+            {
+                lst->data = inhalt;
+            }
+        }
+    }
 }
 
-int main(void){
+int main(void)
+{
 
     nodep Liste = NULL;
 
-
-    insertAt(Liste,0,"hallo mein name ist berhan");
+    insertAt(Liste, 0, "hallo mein name ist berhan");
     printList(Liste);
-    
+
     return 0;
 }
