a87457eae4b4041a3c712e459f9f2ec0c20f4500
Berhan Cabuk <berhan.cabuk@student.hs-rm.de>
2025-01-09 22:59:16 +0100

    cawd

--- python/Blatt2/au4.py
+++ python/Blatt2/au4.py
@@ -1,16 +1,32 @@
+def auskunft(linie, start, ziel):
+    # Datei einlesen
+    with open('fahrzeiten.txt', 'r') as file:
+        daten = file.readlines()
 
-def auskunft(linie,start,ziel):
-    if(start == ziel):
-        return "0"
-    return 0
+    # Relevante Verbindungen für die Linie extrahieren
+    verbindungen = []
+    for zeile in daten:
+        linienname, haltestelle_von, haltestelle_nach, zeit = zeile.strip().split(';')
+        if linienname == linie:
+            verbindungen.append((haltestelle_von, haltestelle_nach, int(zeit)))
+
+    # Fahrplan in Form eines Dictionaries erstellen
+    fahrplan = {start: ziel for start, ziel, _ in verbindungen}
+    zeiten = {start: zeit for start, _, zeit in verbindungen}
+
+    # Route und Gesamtfahrzeit berechnen
+    route = [start]
+    gesamtzeit = 0
+    aktuelle_haltestelle = start
+
+    while aktuelle_haltestelle != ziel:
+        naechste_haltestelle = fahrplan[aktuelle_haltestelle]
+        gesamtzeit += zeiten[aktuelle_haltestelle]
+        route.append(naechste_haltestelle)
+        aktuelle_haltestelle = naechste_haltestelle
+
+    return gesamtzeit, route
 
 
-
-f = open("fahrzeiten.txt","r")
-text = f.read()
-parts = text.split(";")
-
-for line in open("fahrzeiten.txt","r"):
-    linie,halteStelle,naechsteStelle,minuten = line.split(";")
-    text = auskunft(linie,halteStelle,naechsteStelle)
-    print(text)
+minuten, weg = auskunft("S9", "Kelsterbach", "Niederrad")
+print(minuten, "Minuten so:", weg)
